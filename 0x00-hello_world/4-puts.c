13dd7139f8d4f1329c3fc92d4a9e7987a3d2d811
Omboya <oliviamboya1@gmail.com>
2022-10-14 06:14:03 -0700

    C program that prints exactly.....followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main print text
+ * main- print text
  *
  * Return always 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
