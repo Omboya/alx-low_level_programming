d3176fc1487aaa28c3e7719ca03e8bd89923405b
Omboya <oliviamboya1@gmail.com>
2022-10-17 02:26:25 -0700

    program that prints all possible differ

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,10 +20,10 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
-																																			}
-																	}
-									}
-
-				putchar('\n');
-				return (0);
+			}
+		}
+}
+{
+	putchar('\n');
+	return (0);
 }
