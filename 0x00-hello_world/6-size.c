252f421348ff5ddf9100b65edc9e07d8875ad622
Omboya <oliviamboya1@gmail.com>
2022-10-17 05:48:59 -0700

    C program that prints the size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - writes scripts
+ * main - writes scripts of many variables
+ *
+ * Description- pirnitn gof alphabets
  *
  * return always 0
  */
 int main(void)
 {
 	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+		int b;
+		long int c;
+		long long int d;
+		float e;
 
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(a));
-	printf("Size of an int: %ld byte(s)\n", sizeof(b));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
-	printf("Size of a float: %ld byte(s)\n", sizeof(e));
+	{
+		printf("Size of a char: %ld byte(s)\n", sizeof(a));
+		printf("Size of an int: %ld byte(s)\n", sizeof(b));
+		printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+		printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+		printf("Size of a float: %ld byte(s)\n", sizeof(e));
+	}
 	return (0);
 }
