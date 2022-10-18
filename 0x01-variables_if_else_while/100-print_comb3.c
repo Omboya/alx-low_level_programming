f0c54d693b352dc21d0c0bbba69cb293716980a0
Olivia <oliviamboya1@gmail.com>
2022-10-18 12:36:22 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints all possible combinations of two different digits
- * 
+ *
  *
  * Return: Always 0.
  */
@@ -19,7 +19,6 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}
