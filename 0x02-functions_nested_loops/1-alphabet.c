0aa2b36df3c40932e5cd84e0eafb38a39b3c373f
Olivia <oliviamboya1@gmail.com>
2022-10-19 08:54:58 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
 * main - Prints alphabet in lower case
+* Return: success 1
 */
 void print_alphabet(void)
 {
@@ -12,18 +12,3 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }
-=======
-#include "main.h"                                                                                                                
-                                                                                                                                 
-/**                                                                                                                              
-* main - Prints alphabet in lower case                                                                                           
-*/                                                                                                                               
-void print_alphabet(void)                                                                                                        
-{                                                                                                                                
-        char la;                                                                                                                 
-        for  (la ='a'; la <= 'z'; la++)                                                                                          
-                _putchar (la);                                                                                                   
-                                                                                                                                 
-        _putchar('\n');                                                                                                         
-}  
->>>>>>> c5b9100b7413b41b6cfad4899320551df630a488
