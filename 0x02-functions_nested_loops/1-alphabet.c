57f2fa1c5aef1cfbb9be1ba835f701b077fb6db2
Omboya <oliviamboya1@gmail.com>
2022-10-18 23:12:07 -0700

    function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
 * main - Prints alphabet in lower case
+* Return: sucess 1
 */
 void print_alphabet(void)
 {
 	char la;
-	for  (la ='a'; la <= 'z'; la++)
+	for  (la = 'a'; la <= 'z'; la++)
+	{
 		_putchar (la);
 
 	_putchar('\n');
+	}
 }
-=======
-#include "main.h"                                                                                                                
-                                                                                                                                 
-/**                                                                                                                              
-* main - Prints alphabet in lower case                                                                                           
-*/                                                                                                                               
-void print_alphabet(void)                                                                                                        
-{                                                                                                                                
-        char la;                                                                                                                 
-        for  (la ='a'; la <= 'z'; la++)                                                                                          
-                _putchar (la);                                                                                                   
-                                                                                                                                 
-        _putchar('\n');                                                                                                         
-}  
->>>>>>> c5b9100b7413b41b6cfad4899320551df630a488
