561fd7452b6dae20ece4fabff744e9b9c1a2180a
Olivia <oliviamboya1@gmail.com>
2022-10-20 11:50:35 +0300

    Update 100-times_table.c
    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_times_table - Prints a multiplication table up to param
+  * times_table - Prints a multiplication table up to param
   * @n: The number to be treated
   *
   * Return: Number matrix
