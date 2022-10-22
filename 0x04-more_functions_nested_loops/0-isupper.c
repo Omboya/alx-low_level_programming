23fb39693a88cb92af5743f6c5ef5637102657bc
Omboya <oliviamboya1@gmail.com>
2022-10-22 00:02:04 -0700

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * Return: 1 if c is uppercase
+ * 0 otherwise
+ *
+ */
+int _isupper(int c)
+{ 
+	int c
+		c = 'A';
+	{
+		printf("%c: %d\n", c, _isupper(c));
+	}
+	c = 'a';
+	{
+		printf("%c: %d\n", c, _isupper(c));
+	}
+	return (0);
+}
