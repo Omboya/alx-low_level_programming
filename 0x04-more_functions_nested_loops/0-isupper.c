fcde4d1ab395fcbc28f70ef2ce419558f3db0df5
Omboya <oliviamboya1@gmail.com>
2022-10-23 04:46:28 -0700

    Task o

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character.
- * Return: 1 if c is uppercase
- * 0 otherwise
+ * _isupper - checks for uppercase character
+ * @c: the upper character to be checked
  *
+ * Return: 1 if character is uppercase, 0 otherwise
  */
-int _isupper(int c)
-{ 
-	int c
-		c = 'A';
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
 	{
-		printf("%c: %d\n", c, _isupper(c));
+	return (1);
 	}
-	c = 'a';
+	else
 	{
-		printf("%c: %d\n", c, _isupper(c));
-	}
 	return (0);
+	}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,16 +1,10 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_Hi
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-int _putchar(char c);
+/**
+ * main.h - Header containing all the prototypes
+ *
+ */
+int _putchar(int c);
 
 #endif
