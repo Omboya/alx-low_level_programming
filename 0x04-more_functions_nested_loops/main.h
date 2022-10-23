8f46091cb3a13eaa1713457da272e6cb5a552658
Omboya <oliviamboya1@gmail.com>
2022-10-23 08:54:47 -0700

    prints the numbers, from 0 to 9 without 2 and 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,12 +3,15 @@
 
 /**
 * file - main.h
-*Description: Header containing all the prototypes
+* Description: Header containing all the prototypes
 */
 
 int _putchar(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+
 
 #endif
