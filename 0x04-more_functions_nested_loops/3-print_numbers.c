19e7493881211f1dc86fcf3ebb998a0ad6c4f729
Omboya <oliviamboya1@gmail.com>
2022-10-23 06:12:00 -0700

    prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,8 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
- *
+ * 
+ * Return: The numbers from 0 to 9
  */
 void print_numbers(void)
 {
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,14 +2,13 @@
 #define MAIN_H
 
 /**
-* main.h - Header containing all the prototypes
-*
+* file - main.h
+*Description: Header containing all the prototypes
 */
 
 int _putchar(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-
 
 #endif
