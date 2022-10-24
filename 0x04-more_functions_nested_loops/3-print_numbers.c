4608b049e5093c5a58b3ed52c4382f6b697eb728
Omboya <oliviamboya1@gmail.com>
2022-10-24 00:41:20 -0700

    function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
- * 
+ *
  * Return: The numbers from 0 to 9
  */
 void print_numbers(void)
@@ -11,5 +11,5 @@ void print_numbers(void)
 	{
 	_putchar((n % 10) + '0');
 	}
-	_putchar('/n');
+	_putchar('\n');
 }
