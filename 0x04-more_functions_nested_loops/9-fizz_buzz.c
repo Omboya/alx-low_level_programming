19574dce5fd4606588bfc4bd99fac7fe43136943
Omboya <oliviamboya1@gmail.com>
2022-10-23 23:26:44 -0700

    FizzBuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -12,32 +13,30 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-	if ((n % 3) == 0 && (n % 5) == 0)
+	if ((n % 3 == 0) && (n % 5 == 0))
 	{
 	printf("FizzBuzz");
 	}
-	else if ((n % 3) == 0)
+	else if (n % 3 == 0)
 	{
 	printf("Fizz");
 	}
-	else if ((n % 5) == 0)
+	else if (n % 5 == 0)
 	{
 	printf("Buzz");
 	else
 	{
 	printf("%d", n);
 	}
-	if (n == 100)
+	if (n != 100)
 	{
 	continue;
 	{
 	printf(" ");
 	}
-	{
-	printf("\n");
+	}
+	printf("\n");	
 	return (0);
-	}
-	}
 	}
 	}
 }
