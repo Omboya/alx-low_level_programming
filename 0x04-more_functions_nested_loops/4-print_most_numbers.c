f5e72c417f9aacbdc20ef6c8458f5c7da79c28d3
Omboya <oliviamboya1@gmail.com>
2022-10-24 00:51:14 -0700

    that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
 void print_most_numbers(void)
 {
 	int n;
-	for (n = 0; n <= 9; n++)
-		
-	{
-	if (n != 2 && n != 4)
-	{
-	_putchar((n % 10) + '0');
-	}
-	}
-	_putchar('\n');
+
+		for (n = 0; x <= 9; x++)
+		{
+		if (n != 2 && n != 4)
+		{
+			_putchar((n % 10) + '0');
+		}
+		}
+		_putchar('\n');
 }
