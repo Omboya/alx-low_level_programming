796f1de96e217a7c4ad5bc8f7a4aafb00852b33a
Omboya <oliviamboya1@gmail.com>
2022-10-24 01:22:38 -0700

    testing main file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,18 +6,9 @@
 */
 int main(void)
 {
-<<<<<<< HEAD
 	int i;
-	
+
 	i = 0;
 	positive_or_negative(i);
-=======
-int i;
-i = 0;
-
-positive_or_negative(i);
-  
->>>>>>> 3a482c2e2f446f8df57734601fb711eddd19f326
-}
-
-return (0);
+	return (0);
+}    
