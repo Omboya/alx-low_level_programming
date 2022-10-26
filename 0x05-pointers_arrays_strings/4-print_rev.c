e1d610bb0b50436d869b895f03f6aa3aa8a315b4
Omboya <oliviamboya1@gmail.com>
2022-10-26 08:57:49 -0700

    hat prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse,
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int 1 = 0;
+
+	while (s[a] != '\0')
+		a++;
+	for (a = a - 1; a >= 0; a--)
+		_putchar(s[a]);
+
+	_putchar('\n');
+}
