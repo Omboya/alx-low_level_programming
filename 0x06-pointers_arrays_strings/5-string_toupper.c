a7c415eac70836378d52ee9c457eedfe06e672a8
Omboya <oliviamboya1@gmail.com>
2022-10-28 20:29:07 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: string parametre
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+	while (s[i)]
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
