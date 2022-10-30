45d0dc2e268dfc1d22276f6812f54af55417c81b
Omboya <oliviamboya1@gmail.com>
2022-10-30 03:07:22 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dlen = 0, i = 0;
-i
-	while (dest[dlen])
+	int i = 0, dest_len = 0;
+
+	while (dest[i++])
 	{
-		dlen++;
+		dest_len++;
 	}
-	for  (i = 0; src[i] != 0; i++)
+	for  (i = 0; src[i]; i++)
 	{
-		dest[dest_len] = src[i];
-		dlen++;
+		dest[dest_len++] = src[i];
 	}
 	return (dest);
 }
