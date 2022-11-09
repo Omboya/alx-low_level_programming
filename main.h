b225fb3dba71b7e64d9d89cfd9161fd4fc930b03
Omboya <oliviamboya1@gmail.com>
2022-11-09 08:11:21 -0800

    hot fix

--- main.h
+++ main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-void reset_to_98(int *n);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -10,7 +8,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
-char *_strcpy(char *dest, char *s);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
