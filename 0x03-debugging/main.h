d931d7b37588d316b9b2e444ab0ad832da82e102
Omboya <oliviamboya1@gmail.com>
2022-11-12 01:48:57 -0800

    hot fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i)
+void positive_or_negative(int i);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -17,7 +17,7 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 int convert_day(int month, int day);
-int largest_number(int a; int b; int c;);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
 
