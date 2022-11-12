12abaf64ec933eb30cd210d56754fb6795046191
Omboya <oliviamboya1@gmail.com>
2022-11-12 02:46:02 -0800

    prints squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,16 +13,16 @@ void print_square(int size)
 	if (size > 0)
 	{
 		for (a = 0 ; a < size ; a++)
-											{
-															for (b = 0 ; b < size ; b++)
-																			{
-																								_putchar(35);
-																											}
-																		_putchar('\n');
-																				}
-									}
-				else
-						{
-									_putchar('\n');
-										}
+		{
+			for (b = 0 ; b < size ; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
