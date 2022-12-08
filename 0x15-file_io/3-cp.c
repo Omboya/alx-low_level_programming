55ce2f3721d2bacf9fd79c2ed0e794b5cb9fac49
Omboya <oliviamboya1@gmail.com>
2022-12-07 17:44:34 -0800

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,36 +10,33 @@ void close_file(int fd);
  *   * @file: The name of the file buffer is storing chars for.
  *    *
  *     * Return: A pointer to the newly-allocated buffer.
- *      */
+ */
 char *create_buffer(char *file)
 {
-		char *buffer;
+	char *buffer;
 
-			buffer = malloc(sizeof(char) * 1024);
-
-				if (buffer == NULL)
-						{
-									dprintf(STDERR_FILENO,
-															"Error: Can't write to %s\n", file);
-											exit(99);
-												}
-
-					return (buffer);
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buffer);
 }
 
 /**
  *  * close_file - Closes file descriptors.
  *   * @fd: The file descriptor to be closed.
- *    */
+ */
 void close_file(int fd)
 {
-		int c;
+	int c;
 
-			c = close(fd);
-
-				if (c == -1)
-						{
-									dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-											exit(100);
-												}
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
