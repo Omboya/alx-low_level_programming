33bc8817eaa47fa4b30c1150a5cd0fe4a8887d27
Omboya <oliviamboya1@gmail.com>
2022-12-07 17:30:42 -0800

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,8 +7,8 @@
  *    * @letters: The number of letters the
  *     *           function should read and print.
  *      *
- *       * Return: If the function fails or filename is NULL - 0.
- *        *         O/w - the actual number of bytes the function can read and print.
+ * Return: If the function fails or filename is NULL - 0.
+ * O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -16,25 +16,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	char *buffer;
 
-				if (filename == NULL)
-							return (0);
-
-					buffer = malloc(sizeof(char) * letters);
-						if (buffer == NULL)
-									return (0);
-
-							o = open(filename, O_RDONLY);
-								r = read(o, buffer, letters);
-									w = write(STDOUT_FILENO, buffer, r);
-
-										if (o == -1 || r == -1 || w == -1 || w != r)
-												{
-															free(buffer);
-																	return (0);
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
 																		}
-
-											free(buffer);
-												close(o);
-
-													return (w);
+	free(buffer);
+	close(o);
+	return (w);
 }
